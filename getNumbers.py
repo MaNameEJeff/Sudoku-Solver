db81c1f04222fc3beaba88ef40d4fa051caefeeb
unknown <varun.c.blr@gmail.com>
2021-11-05 16:56:24 +0530

    Neater code

--- getNumbers.py
+++ getNumbers.py
@@ -6,29 +6,33 @@ class getNumbers():
 	def main():
 
 		#This part needs to be sent by another program
-		grid = {"Row 1": [cv.imread('row1 square1.png'), cv.imread('row1 square2.png')], "Row 2": [cv.imread('row2 square1.png'), cv.imread('row2 square2.png')]}
+		grid = {
+			"Row 1": [cv.imread('row1 square1.png'), cv.imread('row1 square2.png')],
+			"Row 2": [cv.imread('row2 square1.png'), cv.imread('row2 square2.png')]
+		}
+
 		grid_numbers = {}
 
-		for k,v in grid.items():
+		for key,value in grid.items():
 			numbers = []
 
-			for i in v:
+			for img in value:
 
 				#Use the tesseract OCR to recognize each digit in each image and remove the form feed and new line characters
-				y = pytesseract.image_to_string(i,config='--psm 10')
+				digit = pytesseract.image_to_string(img,config='--psm 10')
 
 				#Remove noise
-				for ch in y:
+				for ch in digit:
 					if ch not in "1234567890":
-						y = y.replace(ch, "")
+						digit = digit.replace(ch, "")
 
 				#Store the digits in grid_numbers, if there is no digit in image store "*"
-				if y == "":
+				if digit == "":
 					numbers.append("*")
 				else:
-					numbers.append(y)
+					numbers.append(digit)
 
-			grid_numbers[k] = numbers
+			grid_numbers[key] = numbers
 
 		print(grid_numbers)
 
