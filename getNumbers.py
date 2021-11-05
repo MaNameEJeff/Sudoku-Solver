a9daacb2948d0c83bd0a6b2f48319535e00be727
unknown <varun.c.blr@gmail.com>
2021-11-05 16:50:03 +0530

    Small bug fixes

--- getNumbers.py
+++ getNumbers.py
@@ -1,28 +1,36 @@
 import pytesseract
 import cv2 as cv
 
-#This part needs to be sent by another program
-grid = {"Row 1": [cv.imread('row1 square1.png'), cv.imread('row1 square2.png')], "Row 2": [cv.imread('row2 square1.png'), cv.imread('row2 square2.png')]}
-grid_numbers = {}
+class getNumbers():
 
-for k,v in grid.items():
-	numbers = []
+	def main():
 
-	for i in v:
+		#This part needs to be sent by another program
+		grid = {"Row 1": [cv.imread('row1 square1.png'), cv.imread('row1 square2.png')], "Row 2": [cv.imread('row2 square1.png'), cv.imread('row2 square2.png')]}
+		grid_numbers = {}
 
-		#Use the tesseract OCR to recognize each digit in each image and remove the form feed and new line characters
-		y = pytesseract.image_to_string(i,config='--psm 10')
+		for k,v in grid.items():
+			numbers = []
 
-		for ch in y:
-			if ch not in "1234567890":
-				y = y.replace(ch, "")
+			for i in v:
 
-		#Store the digits in grid_numbers, if there is no digit in image store "*"
-		if y == "":
-			numbers.append("*")
-		else:
-			numbers.append(y)
+				#Use the tesseract OCR to recognize each digit in each image and remove the form feed and new line characters
+				y = pytesseract.image_to_string(i,config='--psm 10')
 
-	grid_numbers[k] = numbers
+				#Remove noise
+				for ch in y:
+					if ch not in "1234567890":
+						y = y.replace(ch, "")
 
-print(grid_numbers)
+				#Store the digits in grid_numbers, if there is no digit in image store "*"
+				if y == "":
+					numbers.append("*")
+				else:
+					numbers.append(y)
+
+			grid_numbers[k] = numbers
+
+		print(grid_numbers)
+
+	if __name__ == '__main__':
+		main()
