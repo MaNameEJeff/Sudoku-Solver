3e7a8c83dd25d404a35eb55062642ed77d06550b
unknown <varun.c.blr@gmail.com>
2021-11-05 15:14:59 +0530

    OCR works

---
+++ README.md
@@ -0,0 +1,11 @@
+A program to solve sudoku from image
+
+INSTALLATION:
+
+-Install the tessearct ocr from here https://digi.bib.uni-mannheim.de/tesseract/
+
+version used in code - tesseract-ocr-w64-setup-v4.0.0.20181030.exe
+
+-Add tesseract to Path
+
+-Install requirements.txt
---
+++ test.py
@@ -0,0 +1,28 @@
+import pytesseract
+import cv2 as cv
+
+#This part needs to be sent by another program
+grid = {"Row 1": [cv.imread('row1 square1.png'), cv.imread('row1 square2.png')], "Row 2": [cv.imread('row2 square1.png'), cv.imread('row2 square2.png')]}
+grid_numbers = {}
+
+for k,v in grid.items():
+	numbers = []
+
+	for i in v:
+
+		#Use the tesseract OCR to recognize each digit in each image and remove the form feed and new line characters
+		y = pytesseract.image_to_string(i,config='--psm 10')
+
+		for ch in y:
+			if ch not in "1234567890":
+				y = y.replace(ch, "")
+
+		#Store the digits in grid_numbers, if there is no digit in image store "*"
+		if y == "":
+			numbers.append("*")
+		else:
+			numbers.append(y)
+
+	grid_numbers[k] = numbers
+
+print(grid_numbers)
