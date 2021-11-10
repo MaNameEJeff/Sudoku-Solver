a2dd30cf4303e67d3892db6b6f7c87fb9bb28033
unknown <varun.c.blr@gmail.com>
2021-11-10 23:18:19 +0530

    Added comments

--- smallBox.py
+++ smallBox.py
@@ -1,30 +1,34 @@
+#Each 3x3 box in the 9x9 puzzle
+
 class smallBox:
 
 	def __init__(self, location, digits):
+		#Where it is in the whole puzzle
 		self.location = location
-		self.setdigits(digits)
 
-	def setdigits(self, digits):
-		l = []
+		#Set the digits in the 3x3 box
+		rows = []
 		count = 0
 
 		while(count < 10):
-			l.append(digits[count:(count+3)])
+			rows.append(digits[count:(count+3)])
 			count += 3
 
-		l.pop()
-		self.digits = l
+		rows.pop() #The last item will always be [] so remove it from the list of digits in the small box
+		self.digits = rows
 
+	#Change the digit at the location to given number
 	def update(self, loc, number):
 		self.digits[loc[0]][loc[1]] = str(number)
 
+	#Check if the small box has a certain digit or not
 	def has(self, number):
 		for row in self.digits:
 			if number in row:
 				return True
-
 		return False
 
+	#Return the digits of the small box in the form of columns instead of rows
 	def get_columns(self):
 
 		columns = []
@@ -39,6 +43,7 @@ class smallBox:
 
 		return(columns)
 
+	#Check if there are any empty spaces in the small box
 	def get_empty(self):
 
 		locations = []
