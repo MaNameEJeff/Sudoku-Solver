1f5c6a7a8e14df12c35fb1f073a5486293b9de5d
unknown <varun.c.blr@gmail.com>
2021-11-15 19:22:38 +0530

    Now working for all images.

--- cropping.py
+++ cropping.py
@@ -1,23 +1,57 @@
 import cv2 as cv
 import numpy as np
+import math
 
 class cropping():
 
 	def crop_image(self):
 
-		img = cv.imread('Original.png')
-		h = img.shape[0]
-		w = img.shape[1]
-		
-		images = {}
-		
+		img = cv.imread("Test2.png")
+		background = cv.imread("background.png")
+
+		#I dont understand this
+		x_offset = 50
+		y_offset = 50
+		background[y_offset:y_offset+img.shape[0], x_offset:x_offset+img.shape[1]] = img
+
+		img_gray = cv.cvtColor(background, cv.COLOR_BGR2GRAY)		
+		img_canny = cv.Canny(img_gray, 30, 200)
+		contours, hierarchy = cv.findContours(img_canny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+
+		peri = cv.arcLength(contours[0], True) #Gets the perimeter of the contour.
+		approx = cv.approxPolyDP(contours[0], 0.02*peri, True) #Get the corner points
+
+		puzzle = img_gray[approx[0][0][1]:approx[1][0][1], approx[1][0][0]:approx[2][0][0]].copy()
+
+		cv.imshow("puzzle", puzzle)
+		cv.waitKey(0)
+
+		h = puzzle.shape[0]
+		w = puzzle.shape[1]
+
+		side = round((h + w)/2)
+		increment = math.ceil(side/9)
+
+		images = {}		
 		count = 1
-		for y in range(0,225,25):
-			for x in range(0,225,25):
-				box = img[y:y+25, x:x+25].copy()
-				box = cv.cvtColor(box, cv.COLOR_BGR2GRAY)
-				box = cv.GaussianBlur(box, (5, 5), 0.5)
-	
+
+		for y in range(0,side,increment):
+			for x in range(2,side,increment):
+				box = puzzle[y:y+increment, x:x+increment].copy()
+				box = cv.resize(box, None, fx=5, fy=5, interpolation=cv.INTER_CUBIC)
+				kernel = np.ones((1, 1), np.uint8)
+				box = cv.dilate(box, kernel, iterations=2)
+				box = cv.erode(box, kernel, iterations=1)
+				box = cv.threshold(cv.medianBlur(box, 5), 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)[1]
+
+				height = box.shape[1]
+				width = box.shape[0]
+
+				if(count == 9):
+					box = box = box[int(height/10):(box.shape[1] - int(height/10)), int(width/10):(box.shape[0]-int(width/10))].copy()
+				else:
+					box = box[int(height/10):(box.shape[1] - int(height/20)), int(width/10):(box.shape[0]-int(width/10))].copy()
+
 				if(("Row " + str(count)) not in images):
 					images[("Row " + str(count))] = [box]
 				else:
--- getNumbers.py
+++ getNumbers.py
@@ -1,10 +1,14 @@
-import pytesseract
+import easyocr
+import cv2 as cv
 
 class getNumbers():
 
 	def get_numbers(self, images):
 
+		reader = easyocr.Reader(['en'])
+
 		image_numbers = []
+		valid = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '']
 
 		for key,value in images.items():
 			numbers = []
@@ -12,14 +16,22 @@ class getNumbers():
 			for img in value:
 
 				#Use the tesseract OCR to recognize each digit in each image
-				digit = pytesseract.image_to_string(img,config='--psm 10')
+				result = reader.readtext(img)
+				if (len(result) == 0):
+					digit = ""
+				else:
+					digit = result[0][1]
 
 				#Remove noise
 				for ch in digit:
 					if ch not in "1234567890":
 						digit = digit.replace(ch, "")
 
+				if(len(digit) > 1):
+					digit = digit[1:]
+
 				#Store the digits in images_numbers, if there is no digit in image store "*"
+
 				if digit == "":
 					numbers.append("*")
 				else:
